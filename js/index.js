3d6f0239b35aab9fcd517fbb93ca86cd0e3e28da
Thiago Freire <tlfreire@yahoo.com>
2021-06-13 18:14:10 -0300

    first

---
+++ js/index.js
@@ -0,0 +1,60 @@
+window.addEventListener('load', () => {
+  axios.get('http://localhost:3000/users')
+      .then(resposta => {
+          localStorage.setItem("ListaDeUsuarios", JSON.stringify(resposta.data.users));
+      });
+  });
+
+
+function acesso() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  let logado = false;
+
+  const usuariosLS = localStorage.getItem("ListaDeUsuarios");
+  const usuariosFormatados = JSON.parse(usuariosLS);
+  let usuarioLogado = "";
+
+  for (const usuario of usuariosFormatados) {
+    
+    if (usuario.email === email && usuario.senha == password) {
+      logado = true;
+      usuarioLogado = usuario;
+      window.location.href = "http://127.0.0.1:5501/paginaDeRecados.html";
+    }
+    localStorage.setItem("UsuarioLogado", JSON.stringify(usuarioLogado.userId));
+  }
+  if (logado === false) {
+    alert("Dados inválidos. Digite novamente ou crie sua conta.");
+  }
+}
+ function botaoCriarConta() {
+   document.getElementById("entrar").innerHTML = "CRIAR CONTA";
+   document.getElementById("repeat-password").style.display = "inline";
+   document.getElementById("botao-criar-conta-agora").style.display = "inline";
+   document.getElementById("botao-entrar").style.display = "none";
+   document.getElementById("botao-criar-conta").style.display = "none";
+   document.getElementById("email").style.width = "330px";
+   document.getElementById("password").style.width = "330px";
+   document.getElementById("password").style.marginTop = "3px";
+   document.getElementById("password").style.marginBottom = "3px";
+ }
+
+ function criarConta() {
+   const email = document.getElementById("email").value;
+   const password = document.getElementById("password").value;
+   const repeatPassword = document.getElementById("repeat-password").value;
+   
+   if (password != repeatPassword) {
+     return alert("Senhas devem ser idênticas");
+   }
+   
+   axios.post(`http://localhost:3000/users`, {
+        email: email,
+        senha: password
+    }).then(retorno => {
+      localStorage.setItem("ListaDeUsuarios", JSON.stringify(listaDeUsuarios));
+        
+      })
+   location.reload();
+ }
---
+++ js/recados.js
@@ -0,0 +1,115 @@
+
+let usuariosLS = localStorage.getItem("ListaDeUsuarios");
+let usuariosFormatados = JSON.parse(usuariosLS);
+
+let usuarioLogadoLS = localStorage.getItem("UsuarioLogado");
+let usuarioLogadoFormatados = JSON.parse(usuarioLogadoLS);
+
+    function ImprimirDados() {
+        const dados = document.getElementById("corpo");
+        let novoConteudo = "";
+     
+        for (let usuarioX=0; usuarioX < usuariosFormatados.length; usuarioX++) {
+          for(let recadoY=0; recadoY < usuariosFormatados[usuarioX].recados.length; recadoY++)
+          novoConteudo += `
+              <tr>
+                 <td></td>
+                 <td>${usuariosFormatados[usuarioX].recados[recadoY].descricao}</td>
+                 <td>${usuariosFormatados[usuarioX].recados[recadoY].detalhe}</td>
+                 <td><button type="button" onclick='modalAtualizarRecado("${usuariosFormatados[usuarioX].recados[recadoY].recadoId}")' class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Editar</button>
+                 <button type="button" onclick='apagarRecado("${usuariosFormatados[usuarioX].recados[recadoY].recadoId}")' class="btn btn-primary">Apagar</button></td>
+             </tr>
+              `;
+         }
+         novoConteudo += "</tbody> </table>";
+         dados.innerHTML = novoConteudo;
+         console.log(usuariosFormatados[usuarioX].recados[recadoY].descricao)
+      }
+
+    ImprimirDados()
+
+    function salvarDetalhe() {
+   
+      let inputDescricao = document.getElementById("inputDescricao").value;
+      let inputDetalhamento = document.getElementById("inputDetalhamento").value;
+
+      axios.post(`http://localhost:3000/users/${usuarioLogadoFormatados}/recados`, {
+      descricao: inputDescricao,
+      detalhe: inputDetalhamento
+      }).then(retorno => {
+        axios.get('http://localhost:3000/users')
+      .then(resposta => {
+          localStorage.setItem("ListaDeUsuarios", JSON.stringify(resposta.data.users));
+          location.reload();
+          ImprimirDados()
+          
+      });
+        });
+   }
+
+   
+   
+   function apagarRecado(idRecado) {
+
+       let idUsuario = "";
+       for (let usuarioX=0; usuarioX < usuariosFormatados.length; usuarioX++) {
+         for(let recadoY=0; recadoY < usuariosFormatados[usuarioX].recados.length; recadoY++) {
+           if(idRecado === usuariosFormatados[usuarioX].recados[recadoY].recadoId) {
+             idUsuario = usuariosFormatados[usuarioX].userId;
+           }
+         }
+       }
+
+      axios.delete(`http://localhost:3000/users/${idUsuario}/recados/${idRecado}`)
+      .then(retorno => {
+        axios.get('http://localhost:3000/users')
+      .then(resposta => {
+        
+          localStorage.setItem("ListaDeUsuarios", JSON.stringify(resposta.data.users));
+          alert("recado apagado")
+          location.reload();
+          ImprimirDados()
+      });
+        });
+   }
+
+   
+
+   function modalAtualizarRecado(idRecado) {
+      let idUsuario = "";
+      for (let usuarioX=0; usuarioX < usuariosFormatados.length; usuarioX++) {
+        for(let recadoY=0; recadoY < usuariosFormatados[usuarioX].recados.length; recadoY++) {
+          if(idRecado === usuariosFormatados[usuarioX].recados[recadoY].recadoId) {
+            idUsuario = usuariosFormatados[usuarioX].userId;
+            document.getElementById("inputDescricaoModal").value = usuariosFormatados[usuarioX].recados[recadoY].descricao;
+            document.getElementById("inputDetalhamentoModal").value =usuariosFormatados[usuarioX].recados[recadoY].detalhe;
+            
+            localStorage.setItem("modalIdUsuario", JSON.stringify(idUsuario));
+            localStorage.setItem("modalIdRecado", JSON.stringify(idRecado));
+          }
+        } 
+      }
+   }
+
+   function editarRecado() {
+
+    let usuario = localStorage.getItem("modalIdUsuario");
+    let usuarioFormatadoId = JSON.parse(usuario);
+
+    let recado = localStorage.getItem("modalIdRecado");
+    let recadoFormatadoId = JSON.parse(recado);
+
+    axios.put(`http://localhost:3000/users/${usuarioFormatadoId}/recados/${recadoFormatadoId}`, {
+      descricao: document.getElementById("inputDescricaoModal").value,
+      detalhe: document.getElementById("inputDetalhamentoModal").value
+    }).then(retorno => {
+        axios.get('http://localhost:3000/users')
+        .then(resposta => {
+        localStorage.setItem("ListaDeUsuarios", JSON.stringify(resposta.data.users));
+        location.reload();
+        ImprimirDados()
+        });
+      });
+  }
+   
+                 
